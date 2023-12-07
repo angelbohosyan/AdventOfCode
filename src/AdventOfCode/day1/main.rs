dbd7c73afb6a745d9ed046749b4614cbdddd4597
angelbohosyan <angelbohosyan@gmail.com>
2023-12-07 23:40:05 +0200

    refactoring

--- src/AdventOfCode/day1/main.rs
+++ src/AdventOfCode/day1/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io;
+
+///54877
+
+fn main() -> Result<(), io::Error> {
+    let file_path = "input.txt";
+    let result = fs::read_to_string(file_path)?;
+
+    let mut sum = 0;
+    for content in result.lines() {
+        for c in content.chars() {
+            if c >= '0' && c <= '9' {
+                if let Some(result) = c.to_digit(10) {
+                    sum += result * 10;
+                    break
+                }
+            }
+        }
+
+        for c in content.chars().rev() {
+            if c >= '0' && c <= '9' {
+                if let Some(result) = c.to_digit(10) {
+                    sum += result;
+                    break
+                }
+            }
+        }
+    }
+
+    println!("Result is {}", sum);
+
+    Ok(())
 }
